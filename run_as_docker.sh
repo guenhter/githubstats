b1312425693a7f4d65648c2ee6e50ffdf1304029
guenhter <grill.guenther@gmail.com>
2026-06-14 09:58:04 +0200

    Kill existing githubstats container before starting

--- run_as_docker.sh
+++ run_as_docker.sh
@@ -3,8 +3,10 @@ set -euo pipefail
 
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
+docker rm -f githubstats 2>/dev/null || true
+
 docker run \
-    --detach \
+    -d \
     --name githubstats \
     --restart on-failure \
     -v "$SCRIPT_DIR:/workspace" \
